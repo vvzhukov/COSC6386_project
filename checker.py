2e831a0151a713417a0a2d817fb18d172644e09e
Vitalii Zhukov <zhuko.v.italy@gmail.com>
2021-04-24 20:20:04 -0500

    Add files via upload

--- checker.py
+++ checker.py
@@ -5,11 +5,8 @@ import example
 
 for func in getmembers(example, isfunction):
 
-    symfz_ct = SimpleSymbolicFuzzer(func[1])
-    paths = symfz_ct.get_all_paths(symfz_ct.fnenter)
-
-    #asymfz_gcd = AdvancedSymbolicFuzzer(func[1], max_iter=10, max_tries=10, max_depth=10)
-    #paths = asymfz_gcd.get_all_paths(asymfz_gcd.fnenter)
+    asymfz_gcd = AdvancedSymbolicFuzzer(func[1], max_iter=10, max_tries=10, max_depth=10)
+    paths = asymfz_gcd.get_all_paths(asymfz_gcd.fnenter)
 
     print("------------------------------------------------------------")
     print("Function:",func[0],". Found total paths: ", len(paths))
@@ -17,5 +14,5 @@ for func in getmembers(example, isfunction):
     for i in range(len(paths)):
         print("------------------------------")
         print("Path#", i)
-        print("Constraints:", symfz_ct.extract_constraints(paths[i]))
-        print("Solution:", symfz_ct.solve_path_constraint(paths[i]))
+        print("Constraints:", asymfz_gcd.extract_constraints(paths[i].get_path_to_root()))
+        print("Solution:", asymfz_gcd.solve_path_constraint(paths[i].get_path_to_root()))
