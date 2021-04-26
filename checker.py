ace8da0b25eb06e2a79c3d0231a78dd2ee7a956d
Vitalii Zhukov <zhuko.v.italy@gmail.com>
2021-04-25 19:31:20 -0500

    Args parser added

--- checker.py
+++ checker.py
@@ -1,11 +1,43 @@
 from fuzzingbook.SymbolicFuzzer import AdvancedSymbolicFuzzer, SimpleSymbolicFuzzer
 from inspect import getmembers, isfunction
-import example
+import importlib
+import argparse
 
+# Initiate the parser
+parser = argparse.ArgumentParser()
 
-for func in getmembers(example, isfunction):
+# Add long and short argument
+parser.add_argument("--file", "-f", nargs="?", const="example", default="example",  help="file to parse")
+parser.add_argument("--iter_max", "-i", nargs="?", const=10, default=10,  help="set maximum iterations")
+parser.add_argument("--tries_max", "-t", nargs="?", const=10, default=10, help="set maximum tries")
+parser.add_argument("--depth_max", "-d", nargs="?", const=10, default=10, help="set maximum depth")
 
-    asymfz_gcd = AdvancedSymbolicFuzzer(func[1], max_iter=10, max_tries=10, max_depth=10)
+# Read arguments from the command line
+args = parser.parse_args()
+
+# Check for args
+if args.iter_max:
+    print("File: %s" % args.file)
+
+if args.iter_max:
+    print("Set maximum iterations to %s" % args.iter_max)
+
+if args.tries_max:
+    print("Set maximum tries to %s" % args.tries_max)
+
+if args.depth_max:
+    print("Set maximum depth to %s" % args.depth_max)
+
+# The file gets executed upon import, as expected.
+mymodule = importlib.import_module(args.file)
+
+for func in getmembers(mymodule, isfunction):
+
+    asymfz_gcd = AdvancedSymbolicFuzzer(func[1],
+                                        max_iter=int(args.iter_max),
+                                        max_tries=int(args.tries_max),
+                                        max_depth=int(args.depth_max))
+
     paths = asymfz_gcd.get_all_paths(asymfz_gcd.fnenter)
 
     print("------------------------------------------------------------")
